97ce5212708e093ef0e9a5037e23730a77772fdc
davila11 <adriana_11_03@hotmail.comm>
2022-09-27 15:04:41 -0500

    validate-email

--- src/app/Components/sign-in/sign-in.component.ts
+++ src/app/Components/sign-in/sign-in.component.ts
@@ -52,50 +52,7 @@ export class SignInComponent implements OnInit {
     //Create the user and clean all inputs
     if (this.formsigIn.valid) {
       const user = this.formsigIn.value;
-      const localInfo = JSON.parse(localStorage.getItem('users') as string);
-      let confirmUser = localInfo.find(
-        (element: { email: string }) => element.email === user.email
-      );
-      if (confirmUser.email === user.email) {
-        this.confirm = true;
-        console.log('ingresa un email valido');
-      } else{
-        // this.userList.push(user)
-        this.confirm = false;
-        localInfo.push(user);
-        console.log('LocalInfo', localInfo);
-        localStorage.setItem('users', JSON.stringify(localInfo));
-        this.formsigIn = this.FormB.group({
-          name: [null, [Validators.required]], 
-          age: [null, [Validators.required]],
-          gender: [null, [Validators.required]],
-          email: [
-            null,
-            [
-              Validators.required,
-              Validators.pattern(/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/),
-            ],
-          ],
-          password: [
-            null,
-            [
-              Validators.required,
-              Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/),
-            ],
-          ],
-        });
-      }
+      
     }
   }
-
-  // checkEmail(control: { value: string }) {
-  //   let enteredEmail = control.value;
-  //   const localInfo = JSON.parse(localStorage.getItem('users') as string);
-  //   let confirmUser = localInfo.find(
-  //     (element: { email: string }) => element.email === enteredEmail
-  //   );
-  //   return confirmUser.email === enteredEmail && enteredEmail
-  //     ? { requirements: true }
-  //     : null;
-  // }
 }
